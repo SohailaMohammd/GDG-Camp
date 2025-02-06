b7f9902fb15acf0671ee5e34b6ea4b4a02d169e2
SohailaMohammd <160627240+SohailaMohammd@users.noreply.github.com>
2025-02-06 18:20:37 +0200

    Add files via upload

---
+++ Task 4/js/script.js
@@ -0,0 +1,10 @@
+let paragraph = document.getElementById("suc");
+let paragraph2 = document.getElementById("suc2");
+
+let btn = document.getElementById("generate");
+
+btn.onclick = function(){
+    paragraph.style.display="none";
+    paragraph2.style.display="block";
+
+}
